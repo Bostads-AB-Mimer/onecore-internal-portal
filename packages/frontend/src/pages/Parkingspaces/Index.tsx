695f74c927ee0cca822c822520b249c72bb36f79
driatic <fredrik.hasselgren@prototyp.se>
2024-04-08 15:14:29 +0200

    feat: 1229 wip use api to fetch listings

--- packages/frontend/src/pages/Parkingspaces/Index.tsx
+++ packages/frontend/src/pages/Parkingspaces/Index.tsx
@@ -1,72 +1,113 @@
-import {
-    IconButton, Typography,
-} from '@mui/material';
-import type { GridColDef } from '@mui/x-data-grid';
-import Chevron from '@mui/icons-material/ChevronRight';
-import * as React from "react";
+import { IconButton, Typography } from '@mui/material'
+import type { GridColDef } from '@mui/x-data-grid'
+import Chevron from '@mui/icons-material/ChevronRight'
+import * as React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
-import DataGridTable from "../../components/DataGridTable";
+import DataGridTable from '../../components/DataGridTable'
+
+
 
 const ParkingSpaces = () => {
+  const [rows, setData] = useState([])
 
-    const sharedProps = {
-        cellClassName: '',
-        editable: false,
-        flex: 1,
-        headerClassName: 'font-bison-bold text-lg text-fuscous-gray',
+  //todo: for demo only, setup backend connection
+  useEffect(() => {
+    const getListingsWithApplicants = async () => {
+      try {
+        const response = await axios.get(
+          'http://localhost:5020/listings-with-applicants'
+        )
+
+        //todo: use onecore-types
+        const rows = response.data.map((item) => ({
+          address: item.Address,
+          rentalPropertyId: item.RentalObjectCode,
+          numberOfApplicants: item.applicants.length,
+          publishedFrom: formatDateString(item.PublishedFrom),
+          publishedTo: formatDateString(item.PublishedTo),
+        }))
+        setData(rows)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
     }
 
-    const columns: GridColDef[] = [
-        {
-            field: 'address',
-            headerName: 'Bilplats',
-            ...sharedProps
-        },
-        {
-            field: 'rentalPropertyId',
-            headerName: 'Hyresid',
-            ...sharedProps
-        },
-        {
-            field: 'numberOfApplicants',
-            headerName: 'Sökande',
-            ...sharedProps
-        },
-        {
-            field: 'pubishedFrom',
-            headerName: 'Datum publicering',
-            ...sharedProps
-        },
-        {
-            field: 'publishedTo',
-            headerName: 'Datum tilldelning',
-            ...sharedProps
-        },
-        {
-            field: "action",
-            headerName: "",
-            sortable: false,
-            renderCell: () => (
-                <IconButton onClick={() => console.log("todo: implement collapsed view")}>
-                    <Chevron/>
-                </IconButton>
-            ),
-        }
-    ];
+    getListingsWithApplicants()
+  }, [])
 
-    const rows = [
-        { address: 'Bellmansgatan 1', rentalPropertyId: '123-123-123-0201/01', numberOfApplicants: '1', pubishedFrom: '2024-01-01', publishedTo: '2024-01-01' },
-        { address: 'Bellmansgatan 2', rentalPropertyId: '123-123-123-0201/02', numberOfApplicants: '2', pubishedFrom: '2024-01-01', publishedTo: '2024-01-01' },
-        { address: 'Bellmansgatan 3', rentalPropertyId: '123-123-123-0201/03', numberOfApplicants: '3', pubishedFrom: '2024-01-01', publishedTo: '2024-01-01' },
-        { address: 'Bellmansgatan 4', rentalPropertyId: '123-123-123-0201/04', numberOfApplicants: '4', pubishedFrom: '2024-01-01', publishedTo: '2024-01-01' },
-    ];
+  const formatDateString = (dateString: string) => {
+    const date = new Date(dateString)
 
-    return (
-        <div>
-            <Typography className="pb-8" variant="h1">Intresseanmälningar Parkeringsplats</Typography>
-            <DataGridTable columns={columns} rows={rows} getRowId={(row: any) => row.rentalPropertyId}></DataGridTable>
-        </div>
-    )
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+
+    return `${year}-${month.toString().padStart(2, '0')}-${day
+      .toString()
+      .padStart(2, '0')}`
+  }
+
+  const sharedProps = {
+    cellClassName: '',
+    editable: false,
+    flex: 1,
+    headerClassName: 'font-bison-bold text-lg text-fuscous-gray',
+  }
+
+  const columns: GridColDef[] = [
+    {
+      field: 'address',
+      headerName: 'Bilplats',
+      ...sharedProps,
+    },
+    {
+      field: 'rentalPropertyId',
+      headerName: 'Hyresid',
+      ...sharedProps,
+    },
+    {
+      field: 'numberOfApplicants',
+      headerName: 'Sökande',
+      ...sharedProps,
+    },
+    {
+      field: 'publishedFrom',
+      headerName: 'Datum publicering',
+      ...sharedProps,
+    },
+    {
+      field: 'publishedTo',
+      headerName: 'Datum tilldelning',
+      ...sharedProps,
+    },
+    {
+      field: 'action',
+      headerName: '',
+      sortable: false,
+      renderCell: () => (
+        <IconButton
+          onClick={() => console.log('todo: implement collapsed view')}
+        >
+          <Chevron />
+        </IconButton>
+      ),
+    },
+  ]
+
+  return (
+    <div>
+      <Typography className="pb-8" variant="h1">
+        Intresseanmälningar Parkeringsplats
+      </Typography>
+      <DataGridTable
+        columns={columns}
+        rows={rows}
+        getRowId={(row: any) => row.rentalPropertyId}
+      ></DataGridTable>
+    </div>
+  )
 }
 
-export default ParkingSpaces;
+export default ParkingSpaces
