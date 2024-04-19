0f5f3cd29186419a2257c2c0510270bf13cada93
momentiris <momentiris@gmail.com>
2024-04-19 17:10:45 +0200

    scaffold listinting detail request

--- packages/backend/src/services/leasing-service/adapters/core-adapter.ts
+++ packages/backend/src/services/leasing-service/adapters/core-adapter.ts
@@ -1,5 +1,5 @@
 import Config from '../../../common/config'
-import {getFromCore} from "../../common/adapters/core-adapter";
+import { getFromCore } from '../../common/adapters/core-adapter'
 
 const coreBaseUrl = Config.core.url
 
@@ -7,9 +7,16 @@ const getListingsWithApplicants = async () => {
   const url = `${coreBaseUrl}/listings-with-applicants`
   const listingsResponse = await getFromCore({
     method: 'get',
-    url: url
+    url: url,
   })
 
   return listingsResponse.data
 }
-export { getListingsWithApplicants }
+
+const getListingWithApplicants = (id: string) =>
+  getFromCore({
+    method: 'get',
+    url: `${coreBaseUrl}/listing-with-applicants/${id}`,
+  }).then((res) => res.data)
+
+export { getListingsWithApplicants, getListingWithApplicants }
--- packages/backend/src/services/leasing-service/index.ts
+++ packages/backend/src/services/leasing-service/index.ts
@@ -1,10 +1,16 @@
 import KoaRouter from '@koa/router'
-import {getListingsWithApplicants} from "./adapters/core-adapter";
+import * as coreAdapter from './adapters/core-adapter'
 
 export const routes = (router: KoaRouter) => {
   router.get('(.*)/leases/listings-with-applicants', async (ctx) => {
-    const listings = await getListingsWithApplicants()
+    const listings = await coreAdapter.getListingsWithApplicants()
 
     ctx.body = listings
+  })
+
+  router.get('(.*)/leases/listing-with-applicants/:id', async (ctx) => {
+    const listing = await coreAdapter.getListingWithApplicants(ctx.params.id)
+
+    ctx.body = listing
   })
 }
