78313240f41163c1e3f888974fc6e51323de9cd5
driatic <fredrik.hasselgren@prototyp.se>
2024-10-22 15:23:19 +0200

    MIM-158: wip

--- packages/frontend/src/pages/ParkingSpace/components/OfferRound.tsx
+++ packages/frontend/src/pages/ParkingSpace/components/OfferRound.tsx
@@ -1,6 +1,11 @@
 import { Box, Chip } from '@mui/material'
 import type { GridColDef } from '@mui/x-data-grid'
-import { ApplicantStatus, LeaseStatus, OfferApplicant } from 'onecore-types'
+import {
+  ApplicantStatus,
+  LeaseStatus,
+  Offer,
+  OfferApplicant,
+} from 'onecore-types'
 
 import { DataGridTable } from '../../../components'
 import { OfferRoundActions } from './OfferRoundActions'
@@ -8,7 +13,127 @@ import { OfferRoundActions } from './OfferRoundActions'
 export const OfferRound = (props: {
   numRound: number
   applicants: Array<OfferApplicant>
+  offer: Offer
 }) => {
+  const sharedProps = {
+    editable: false,
+    flex: 1,
+  }
+
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Namn',
+      ...sharedProps,
+      flex: 1.25,
+      renderCell: (params) => (
+        <Box style={{ display: 'flex', flexDirection: 'column' }}>
+          <Box>{params.row.name}</Box>{' '}
+          <Box>{params.row.nationalRegistrationNumber}</Box>
+        </Box>
+      ),
+    },
+    {
+      field: 'contactCode',
+      headerName: 'Kundnummer',
+      ...sharedProps,
+      flex: 1.25,
+    },
+    {
+      field: 'queuePoints',
+      headerName: 'Köpoäng',
+      ...sharedProps,
+      flex: 0.75,
+    },
+    {
+      field: 'address',
+      headerName: 'Boendeadress',
+      valueGetter: (v) => v.row.address,
+      ...sharedProps,
+      flex: 1.25,
+    },
+    {
+      field: 'housingLeaseStatus',
+      headerName: 'Status Boendekontrakt',
+      valueFormatter: (v) => formatLeaseStatus(v.value),
+      renderCell: (v) => <Chip label={v.formattedValue} />,
+      ...sharedProps,
+      flex: 1,
+    },
+    {
+      field: 'applicationDate',
+      headerName: 'Anmälan',
+      ...sharedProps,
+      valueFormatter: (v) => dateFormatter.format(new Date(v.value)),
+    },
+    {
+      field: 'hasParkingSpace',
+      headerName: 'Har bilplats',
+      valueFormatter: (v) => (v.value ? 'Ja' : 'Nej'),
+      ...sharedProps,
+      flex: 0.75,
+    },
+    {
+      field: 'status',
+      headerName: 'Status ansökan',
+      ...sharedProps,
+      flex: 1.25,
+      valueFormatter: (v) => formatApplicantStatus(v.value),
+      renderCell: (v) => <Chip label={v.formattedValue} />,
+    },
+    {
+      field: 'statusResponse', // A unique field key that refers to the same `status` field
+      headerName: 'Svar erbj.',
+      renderCell: (v) => formatApplicantStatusResponse(v.row.status), // Different logic for rendering the same data
+      ...sharedProps,
+    },
+    {
+      field: 'expiresAt',
+      headerName: 'Svara senast',
+      valueFormatter: (v) =>
+        dateFormatter.format(new Date(props.offer.expiresAt)),
+    },
+    {
+      field: 'applicationType',
+      headerName: 'Ärende',
+      renderCell: (v) => {
+        if (v.value === 'Additional')
+          return v.row.hasParkingSpace ? 'Hyra flera' : 'Hyra en'
+        else return 'Byte'
+      },
+      ...sharedProps,
+    },
+    {
+      field: 'priority',
+      headerName: 'Prioritetsgrupp',
+      ...sharedProps,
+      valueFormatter: (v) => formatApplicantStatus(v.value),
+      renderCell: (v) => v.value ?? <i>N/A</i>,
+    },
+    {
+      field: 'action',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      align: 'center',
+      renderCell: (v) => {
+        if (v.row.status !== ApplicantStatus.Offered) {
+          return null
+        }
+
+        return (
+          <OfferRoundActions
+            disabled={v.row.status !== ApplicantStatus.Offered}
+            listingId={v.row.listingId}
+            applicantName={v.row.name}
+            offerId={v.row.offerId}
+          />
+        )
+      },
+    },
+  ]
+
   return (
     <>
       <DataGridTable
@@ -27,119 +152,7 @@ export const OfferRound = (props: {
   )
 }
 
-const sharedProps = {
-  editable: false,
-  flex: 1,
-}
-
 const dateFormatter = new Intl.DateTimeFormat('sv-SE', { timeZone: 'UTC' })
-const columns: GridColDef[] = [
-  {
-    field: 'name',
-    headerName: 'Namn',
-    ...sharedProps,
-    flex: 1.25,
-    renderCell: (params) => (
-      <Box style={{ display: 'flex', flexDirection: 'column' }}>
-        <Box>{params.row.name}</Box>{' '}
-        <Box>{params.row.nationalRegistrationNumber}</Box>
-      </Box>
-    ),
-  },
-  {
-    field: 'contactCode',
-    headerName: 'Kundnummer',
-    ...sharedProps,
-    flex: 1.25,
-  },
-  {
-    field: 'queuePoints',
-    headerName: 'Köpoäng',
-    ...sharedProps,
-    flex: 0.75,
-  },
-  {
-    field: 'address',
-    headerName: 'Boendeadress',
-    valueGetter: (v) => v.row.address,
-    ...sharedProps,
-    flex: 1.25,
-  },
-  {
-    field: 'housingLeaseStatus',
-    headerName: 'Status Boendekontrakt',
-    valueFormatter: (v) => formatLeaseStatus(v.value),
-    renderCell: (v) => <Chip label={v.formattedValue} />,
-    ...sharedProps,
-    flex: 1,
-  },
-  {
-    field: 'applicationDate',
-    headerName: 'Anmälan',
-    ...sharedProps,
-    valueFormatter: (v) => dateFormatter.format(new Date(v.value)),
-  },
-  {
-    field: 'hasParkingSpace',
-    headerName: 'Har bilplats',
-    valueFormatter: (v) => (v.value ? 'Ja' : 'Nej'),
-    ...sharedProps,
-    flex: 0.75,
-  },
-  {
-    field: 'status',
-    headerName: 'Status ansökan',
-    ...sharedProps,
-    flex: 1.25,
-    valueFormatter: (v) => formatApplicantStatus(v.value),
-    renderCell: (v) => <Chip label={v.formattedValue} />,
-  },
-  {
-    field: 'foo',
-    headerName: 'Svar erbj.',
-    renderCell: (v) => v.value || <i>N/A</i>,
-    ...sharedProps,
-  },
-  {
-    field: 'applicationType',
-    headerName: 'Ärende',
-    renderCell: (v) => {
-      if (v.value === 'Additional')
-        return v.row.hasParkingSpace ? 'Hyra flera' : 'Hyra en'
-      else return 'Byte'
-    },
-    ...sharedProps,
-  },
-  {
-    field: 'priority',
-    headerName: 'Prioritetsgrupp',
-    ...sharedProps,
-    valueFormatter: (v) => formatApplicantStatus(v.value),
-    renderCell: (v) => v.value ?? <i>N/A</i>,
-  },
-  {
-    field: 'action',
-    headerName: '',
-    sortable: false,
-    filterable: false,
-    disableColumnMenu: true,
-    align: 'center',
-    renderCell: (v) => {
-      if (v.row.status !== ApplicantStatus.Offered) {
-        return null
-      }
-
-      return (
-        <OfferRoundActions
-          disabled={v.row.status !== ApplicantStatus.Offered}
-          listingId={v.row.listingId}
-          applicantName={v.row.name}
-          offerId={v.row.offerId}
-        />
-      )
-    },
-  },
-]
 
 const applicantStatusFormatMap: Record<ApplicantStatus, string> = {
   [ApplicantStatus.Active]: 'Aktiv',
@@ -155,6 +168,20 @@ const applicantStatusFormatMap: Record<ApplicantStatus, string> = {
 
 const formatApplicantStatus = (v: ApplicantStatus) =>
   applicantStatusFormatMap[v]
+
+const applicantStatusResponseMap: Record<ApplicantStatus, string> = {
+  [ApplicantStatus.Active]: '',
+  [ApplicantStatus.Assigned]: '',
+  [ApplicantStatus.AssignedToOther]: '',
+  [ApplicantStatus.WithdrawnByUser]: '',
+  [ApplicantStatus.WithdrawnByManager]: '',
+  [ApplicantStatus.Offered]: 'Inväntar svar',
+  [ApplicantStatus.OfferAccepted]: 'Ja',
+  [ApplicantStatus.OfferDeclined]: 'Nej',
+  [ApplicantStatus.OfferExpired]: 'Nej',
+}
+const formatApplicantStatusResponse = (v: ApplicantStatus) =>
+  applicantStatusResponseMap[v] || ''
 
 const leaseStatusFormatMap: Record<LeaseStatus, string> = {
   [LeaseStatus.Current]: 'Aktivt',
--- packages/frontend/src/pages/ParkingSpace/index.tsx
+++ packages/frontend/src/pages/ParkingSpace/index.tsx
@@ -1,7 +1,8 @@
-import { Box, Typography } from '@mui/material'
+import { Box, Chip, Typography } from '@mui/material'
 import { Suspense, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { TabContext, TabPanel } from '@mui/lab'
+import { ApplicantStatus, ListingStatus, OfferStatus } from 'onecore-types'
 
 import { PageGoBackTo, Tab, Tabs } from '../../components'
 import {
@@ -43,11 +44,58 @@ const ParkingSpaceTabs = (props: { listingId: number }) => {
   const handleChange = (_e: React.SyntheticEvent, tab: string) =>
     setSelectedTab(tab)
 
+  const listingFormatMap: Record<ListingStatus, string> = {
+    [ListingStatus.Active]: 'Publicerad',
+    [ListingStatus.Assigned]: 'Tilldelad',
+    [ListingStatus.Deleted]: 'Borttagen',
+    //todo: what is the translation here?
+    //todo: the listing will be in the "Klara för erbjudande" tab
+    //todo: when an offer exists offerFormatMap will be used instead
+    [ListingStatus.Expired]: 'Klar för erbjudande',
+  }
+
+  const offerFormatMap: Record<OfferStatus, string> = {
+    [OfferStatus.Active]: 'Aktiv',
+    [OfferStatus.Accepted]: 'Tilldelad / kontrakterad',
+    [OfferStatus.Declined]: 'Nekad',
+    [OfferStatus.Expired]: 'Utgången',
+  }
+
+  //todo do we need to be this granular?
+  const applicantStatusFormatMap: Record<ApplicantStatus, string> = {
+    [ApplicantStatus.Active]: '',
+    [ApplicantStatus.Assigned]: '',
+    [ApplicantStatus.AssignedToOther]: '',
+    [ApplicantStatus.WithdrawnByUser]: '',
+    [ApplicantStatus.WithdrawnByManager]: '',
+    [ApplicantStatus.Offered]: '',
+    [ApplicantStatus.OfferAccepted]: '',
+    [ApplicantStatus.OfferDeclined]: '',
+    [ApplicantStatus.OfferExpired]: '',
+  }
+
+  const formatStatus = (v: any) => {
+    console.log(data)
+    if (data.offers.length > 0) {
+      return offerFormatMap[data.offers[data.offers.length - 1].status]
+    }
+    return listingFormatMap[data.status]
+  }
+
   return (
     <TabContext value={selectedTab}>
-      <Typography paddingBottom="0.5rem" variant="h1">
-        Intresseanmälningar {data.address}
-      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <Typography paddingBottom="0.5rem" marginRight="1rem" variant="h1">
+          <span>Intresseanmälningar {data.address}</span>
+        </Typography>
+        <Chip label={formatStatus(data)} sx={{ marginY: 'auto' }}></Chip>
+      </Box>
+
       <Tabs onChange={handleChange}>
         <Tab disableRipple label="Alla sökande" value="1" />
         {data.offers.map((offer, i) => (
@@ -68,6 +116,7 @@ const ParkingSpaceTabs = (props: { listingId: number }) => {
             <OfferRound
               key={offer.id}
               applicants={offer.selectedApplicants}
+              offer={offer}
               numRound={i + 1}
             />
           </TabPanel>
