be8ffc60aff5423374038a3392e2244fbbdebc85
momentiris <momentiris@gmail.com>
2024-10-29 14:21:40 +0100

    align loading containers with resulting text

--- packages/frontend/src/pages/ParkingSpaces/components/create-applicant-for-listing/ContactInfoLoading.tsx
+++ packages/frontend/src/pages/ParkingSpaces/components/create-applicant-for-listing/ContactInfoLoading.tsx
@@ -10,34 +10,76 @@ export const ContactInfoLoading = () => (
     justifyContent="space-between"
     flex="1"
   >
-    <Box width="100%">
+    <Box flex="1">
       <ContactInfoRow
         label="Namn"
-        value={<Skeleton variant="rectangular" width="125px" />}
+        value={
+          <Skeleton
+            variant="rectangular"
+            width="125px"
+            sx={{ marginTop: '7px' }}
+          />
+        }
       />
       <ContactInfoRow
         label="Personnummer"
-        value={<Skeleton variant="rectangular" width="100px" />}
+        value={
+          <Skeleton
+            variant="rectangular"
+            width="100px"
+            sx={{ marginTop: '7px' }}
+          />
+        }
       />
       <ContactInfoRow
         label="Kundnummer"
-        value={<Skeleton variant="rectangular" width="100px" />}
+        value={
+          <Skeleton
+            variant="rectangular"
+            width="100px"
+            sx={{ marginTop: '7px' }}
+          />
+        }
       />
       <ContactInfoRow
         label="Adress"
-        value={<Skeleton variant="rectangular" width="150px" />}
+        value={
+          <Skeleton
+            variant="rectangular"
+            width="150px"
+            sx={{ marginTop: '7px' }}
+          />
+        }
       />
       <ContactInfoRow
         label="Område"
-        value={<Skeleton variant="rectangular" width="75px" />}
+        value={
+          <Skeleton
+            variant="rectangular"
+            width="75px"
+            sx={{ marginTop: '7px' }}
+          />
+        }
       />
       <ContactInfoRow
         label="Kontrakt"
-        value={<Skeleton variant="rectangular" width="25px" />}
+        value={
+          <Skeleton
+            variant="rectangular"
+            width="25px"
+            sx={{ marginTop: '7px' }}
+          />
+        }
       />
       <ContactInfoRow
         label="Köpoäng"
-        value={<Skeleton variant="rectangular" width="50px" />}
+        value={
+          <Skeleton
+            variant="rectangular"
+            width="50px"
+            sx={{ marginTop: '7px' }}
+          />
+        }
       />
     </Box>
   </Box>
