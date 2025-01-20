8231a023ccc44ff0c4e657b1449c04fe92845ac7
Adam Agnaou <adam.agnaou@prototyp.se>
2025-01-20 14:56:29 +0100

    feat: add Apartment component and restructure CurrentTypeOfHousingForm

--- packages/frontend/src/pages/Residences/components/CurrentTypeOfHousingForm/index.tsx
+++ packages/frontend/src/pages/Residences/components/CurrentTypeOfHousingForm/index.tsx
@@ -1,58 +1,14 @@
-import {
-  FormControl,
-  Grid,
-  MenuItem,
-  Select,
-  TextField,
-  Typography,
-} from '@mui/material'
+import { FormControl, MenuItem, Select, Typography } from '@mui/material'
 import React, { useState } from 'react'
 
-const OwnsVilla: React.FC = () => {
-  return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6} margin={0}>
-        <FormControl fullWidth>
-          <Typography paddingBottom={1} variant="h2">
-            Antal vuxna i hushåll *
-          </Typography>
-
-          <TextField
-            type="number"
-            name="adult-count"
-            variant="outlined"
-            size="small"
-            defaultValue={1}
-            InputProps={{ inputProps: { min: 1 } }}
-          />
-        </FormControl>
-      </Grid>
-
-      <Grid item xs={12} sm={6} margin={0}>
-        <FormControl fullWidth>
-          <Typography paddingBottom={1} variant="h2">
-            Antal barn i hushåll *
-          </Typography>
-
-          <TextField
-            type="number"
-            name="children-count"
-            variant="outlined"
-            size="small"
-            defaultValue={0}
-            InputProps={{ inputProps: { min: 0 } }}
-          />
-        </FormControl>
-      </Grid>
-    </Grid>
-  )
-}
+import Apartment from './Apartment'
+import OwnsVilla from './OwnsVilla'
 
 const tabs: {
   [key: string]: JSX.Element | undefined
 } = {
-  'rental-apartment': undefined,
-  'sublet-apartment': undefined,
+  'rental-apartment': <Apartment />,
+  'sublet-apartment': <Apartment />,
   'lives-with-parents': undefined,
   'living-with-tenant': undefined,
   'owns-villa': <OwnsVilla />,
