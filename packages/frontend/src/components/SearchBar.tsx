76e0e1cd77afcbacefad6619fe59ee043a337e42
Adam Agnaou <adam.agnaou@prototyp.se>
2025-02-04 11:26:40 +0100

    fix: Restore button appearence

--- packages/frontend/src/components/SearchBar.tsx
+++ packages/frontend/src/components/SearchBar.tsx
@@ -1,5 +1,7 @@
 import { TextField } from '@mui/material'
 
+import { mdTheme } from '../theme'
+
 type SearchBarProps = {
   onChange: (v: string) => void
   disabled?: boolean
@@ -27,5 +29,30 @@ export const SearchBar = ({
     onChange={(event) => onChange(event.currentTarget.value)}
     onFocus={onFocus}
     onBlur={onBlur}
+    sx={{
+      width: '100%',
+      maxWidth: 350,
+      '& .MuiOutlinedInput-root': {
+        fontSize: '16px',
+        paddingTop: '2px',
+        paddingBottom: '2px',
+        color: '#000',
+        '& .MuiOutlinedInput-notchedOutline': {
+          borderColor: mdTheme.palette.warmGrey.main,
+          borderRadius: '6px',
+          borderWidth: '1.5px',
+        },
+        '&.Mui-focused': {
+          '& .MuiOutlinedInput-notchedOutline': {
+            borderWidth: '1.5px',
+            borderColor: '#2e2e2e',
+          },
+        },
+        '& .MuiInputLabel-outlined': {
+          color: '#2e2e2e',
+          '&.Mui-focused': {},
+        },
+      },
+    }}
   />
 )
--- packages/frontend/src/pages/ParkingSpaces/components/create-applicant-for-listing/SearchContact.tsx
+++ packages/frontend/src/pages/ParkingSpaces/components/create-applicant-for-listing/SearchContact.tsx
@@ -10,6 +10,7 @@ import { useState, useMemo, useCallback } from 'react'
 import * as utils from '../../../../utils'
 import { useSearchContacts } from '../../hooks/useSearchContacts'
 import { ContactSearchData } from './types'
+import { mdTheme } from '../../../../theme'
 
 type SearchContactProps = {
   placeholder?: string
@@ -69,6 +70,29 @@ export const SearchContact = ({
             fullWidth
           />
         )}
+        sx={{
+          '& .MuiOutlinedInput-root': {
+            fontSize: '16px',
+            paddingTop: '2px',
+            paddingBottom: '2px',
+            color: '#000',
+            '& .MuiOutlinedInput-notchedOutline': {
+              borderColor: mdTheme.palette.warmGrey.main,
+              borderRadius: '6px',
+              borderWidth: '1.5px',
+            },
+            '&.Mui-focused': {
+              '& .MuiOutlinedInput-notchedOutline': {
+                borderWidth: '1.5px',
+                borderColor: '#2e2e2e',
+              },
+            },
+            '& .MuiInputLabel-outlined': {
+              color: '#2e2e2e',
+              '&.Mui-focused': {},
+            },
+          },
+        }}
       />
     </Box>
   )
