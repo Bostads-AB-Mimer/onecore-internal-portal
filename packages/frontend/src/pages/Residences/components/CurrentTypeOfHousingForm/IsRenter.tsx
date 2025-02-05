45dafa51ea7baedb7756e2d29e43749cdc3ade4b
Adam Agnaou <adam.agnaou@prototyp.se>
2025-02-05 16:20:51 +0100

    refactor: Move FormControl up the heirarchy

--- packages/frontend/src/pages/Residences/components/CurrentTypeOfHousingForm/IsRenter.tsx
+++ packages/frontend/src/pages/Residences/components/CurrentTypeOfHousingForm/IsRenter.tsx
@@ -11,35 +11,37 @@ const IsRenter: React.FC<Props> = ({ control }) => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <FormControl fullWidth>
-          <Typography paddingBottom={1} variant="h2">
-            Hyresvärd *
-          </Typography>
+        <Typography paddingBottom={1} variant="h2">
+          Hyresvärd *
+        </Typography>
 
-          <Controller
-            name="landlord"
-            control={control}
-            render={({ field }) => (
+        <Controller
+          name="landlord"
+          control={control}
+          shouldUnregister
+          render={({ field }) => (
+            <FormControl fullWidth>
               <TextField
                 size="small"
                 placeholder="Namn på nuvarande hyresvärd"
                 {...field}
               />
-            )}
-          />
-        </FormControl>
+            </FormControl>
+          )}
+        />
       </Grid>
 
       <Grid item xs={12} sm={6} margin={0}>
-        <FormControl fullWidth>
-          <Typography paddingBottom={1} variant="h2">
-            Antal vuxna i hushåll *
-          </Typography>
+        <Typography paddingBottom={1} variant="h2">
+          Antal vuxna i hushåll *
+        </Typography>
 
-          <Controller
-            name="numAdults"
-            control={control}
-            render={({ field }) => (
+        <Controller
+          name="numAdults"
+          control={control}
+          shouldUnregister
+          render={({ field }) => (
+            <FormControl fullWidth>
               <TextField
                 type="number"
                 size="small"
@@ -47,21 +49,22 @@ const IsRenter: React.FC<Props> = ({ control }) => {
                 InputProps={{ inputProps: { min: 1 } }}
                 {...field}
               />
-            )}
-          />
-        </FormControl>
+            </FormControl>
+          )}
+        />
       </Grid>
 
       <Grid item xs={12} sm={6} margin={0}>
-        <FormControl fullWidth>
-          <Typography paddingBottom={1} variant="h2">
-            Antal barn i hushåll *
-          </Typography>
+        <Typography paddingBottom={1} variant="h2">
+          Antal barn i hushåll *
+        </Typography>
 
-          <Controller
-            name="numChildren"
-            control={control}
-            render={({ field }) => (
+        <Controller
+          name="numChildren"
+          control={control}
+          shouldUnregister
+          render={({ field }) => (
+            <FormControl fullWidth>
               <TextField
                 type="number"
                 size="small"
@@ -69,41 +72,43 @@ const IsRenter: React.FC<Props> = ({ control }) => {
                 InputProps={{ inputProps: { min: 0 } }}
                 {...field}
               />
-            )}
-          />
-        </FormControl>
+            </FormControl>
+          )}
+        />
       </Grid>
 
       <Grid item xs={12}>
-        <FormControl fullWidth>
-          <Typography paddingBottom={1} variant="h2">
-            Telefonnummer hyresvärd *
-          </Typography>
+        <Typography paddingBottom={1} variant="h2">
+          Telefonnummer hyresvärd *
+        </Typography>
 
-          <Controller
-            name="phone"
-            control={control}
-            render={({ field }) => (
+        <Controller
+          name="phone"
+          control={control}
+          shouldUnregister
+          render={({ field }) => (
+            <FormControl fullWidth>
               <TextField size="small" type="tel" {...field} />
-            )}
-          />
-        </FormControl>
+            </FormControl>
+          )}
+        />
       </Grid>
 
       <Grid item xs={12}>
-        <FormControl fullWidth>
-          <Typography paddingBottom={1} variant="h2">
-            Mejladress hyresvärd
-          </Typography>
+        <Typography paddingBottom={1} variant="h2">
+          Mejladress hyresvärd
+        </Typography>
 
-          <Controller
-            name="email"
-            control={control}
-            render={({ field }) => (
+        <Controller
+          name="email"
+          control={control}
+          shouldUnregister
+          render={({ field }) => (
+            <FormControl fullWidth>
               <TextField size="small" type="email" {...field} />
-            )}
-          />
-        </FormControl>
+            </FormControl>
+          )}
+        />
       </Grid>
     </Grid>
   )
