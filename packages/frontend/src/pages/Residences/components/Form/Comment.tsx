809d195344eb2c5e2be8118e1d4fd4e6c62c14fb
Adam Agnaou <adam.agnaou@prototyp.se>
2025-02-06 15:06:15 +0100

    refactor: Update Comment component to use useFormContext for improved form handling

--- packages/frontend/src/pages/Residences/components/Form/Comment.tsx
+++ packages/frontend/src/pages/Residences/components/Form/Comment.tsx
@@ -1,24 +1,26 @@
 import { FormControl, TextareaAutosize, Typography } from '@mui/material'
 import React from 'react'
-import { Control, Controller } from 'react-hook-form'
+import { Controller, useFormContext } from 'react-hook-form'
 
-interface CommentProps {
-  control: Control<any>
+const Comment: React.FC = () => {
+  const { control } = useFormContext()
+
+  return (
+    <Controller
+      name="housingReference.comment"
+      control={control}
+      defaultValue=""
+      shouldUnregister
+      render={({ field }) => (
+        <FormControl fullWidth>
+          <Typography variant="h2" paddingBottom={1}>
+            Notering/kommentar
+          </Typography>
+          <TextareaAutosize {...field} minRows={3} />
+        </FormControl>
+      )}
+    />
+  )
 }
-
-const Comment: React.FC<CommentProps> = ({ control }) => (
-  <Controller
-    name="housingReference.comment"
-    control={control}
-    render={({ field }) => (
-      <FormControl fullWidth>
-        <Typography variant="h2" paddingBottom={1}>
-          Notering/kommentar
-        </Typography>
-        <TextareaAutosize {...field} minRows={3} />
-      </FormControl>
-    )}
-  />
-)
 
 export default Comment
