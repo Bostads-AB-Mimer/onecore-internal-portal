7489b321889bc0294e440c4686906fcc1efa0bda
Adam Agnaou <adam.agnaou@prototyp.se>
2025-03-12 10:50:53 +0100

    Refactor useCustomerCard to use apiClient

--- packages/frontend/src/pages/Residences/hooks/useCustomerCard.ts
+++ packages/frontend/src/pages/Residences/hooks/useCustomerCard.ts
@@ -1,33 +1,24 @@
-import axios, { AxiosError } from 'axios'
-import { useQuery } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+import { useQuery, UseQueryResult } from '@tanstack/react-query'
 import { schemas } from 'onecore-types'
 import { z } from 'zod'
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL || '/api'
+import apiClient from '../../../utils/api-client'
 
 type CustomerCard = {
   applicationProfile: z.infer<typeof schemas.v1.ApplicationProfileSchema>
 }
 
-export const useCustomerCard = (contactCode: string) =>
+export const useCustomerCard = (
+  contactCode?: string
+): UseQueryResult<CustomerCard, AxiosError> =>
   useQuery<CustomerCard, AxiosError>({
     queryKey: ['customer-card', contactCode],
     enabled: Boolean(contactCode),
-    queryFn: async () =>
-      await axios
-        .get(`${backendUrl}/tbd`, {
-          headers: {
-            Accept: 'application/json',
-            'Access-Control-Allow-Credentials': true,
-          },
-          withCredentials: true,
-        })
-        .then((res) => res.data),
-    retry: (failureCount: number, error: AxiosError) => {
-      if (error.response?.status === 401) {
-        return false
-      } else {
-        return failureCount < 3
-      }
+    queryFn: async () => {
+      const f = await apiClient.get(`/contacts/${contactCode}/customer-card`)
+      return f.data.content
     },
+    retry: (failureCount: number, error: AxiosError) =>
+      error.response?.status === 401 ? false : failureCount < 3,
   })
