a5ef09d37273365880ebb20658e47ec8cc0acb35
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2025-03-18 12:11:37 +0100

    fix: use login (#88)
    * use login
    * Fixed ports for local env
    
    ---------
    
    Co-authored-by: Anders Norrback Bornholm <anders@bornholm.se>

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -2,7 +2,7 @@ import { logger } from 'onecore-utilities'
 
 import app from './app'
 
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 7001
 app.listen(PORT, () => {
   logger.info(`listening on http://localhost:${PORT}`)
 })
