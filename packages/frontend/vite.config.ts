34b1a6be49e06cf58fc9caad4d776d944ffa709a
Anders Norrback Bornholm <anders@bornholm.se>
2025-03-18 12:10:21 +0100

    Fixed ports for local env

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 7003,
     proxy: {
       '/api': {
-        target: `http://localhost:${process.env.PROXY_TARGET_PORT || 7000}`,
+        target: `http://localhost:${process.env.PROXY_TARGET_PORT || 7001}`,
         changeOrigin: true,
         secure: false,
       },
