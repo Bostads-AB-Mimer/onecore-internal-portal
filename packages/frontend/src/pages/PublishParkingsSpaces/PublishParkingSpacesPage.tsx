290428f7ecdb618272c5fb90b318e347d997eeca
0x4d <0x4d@undefined>
2025-05-15 16:10:07 +0200

    Implement PublishParkingSpacesPage with data grid and action buttons for listings

--- packages/frontend/src/pages/PublishParkingsSpaces/PublishParkingSpacesPage.tsx
+++ packages/frontend/src/pages/PublishParkingsSpaces/PublishParkingSpacesPage.tsx
@@ -1,7 +1,150 @@
-import React from 'react'
+import { IconButton, Stack, Typography } from '@mui/material'
+import { type GridColDef } from '@mui/x-data-grid'
+import Chevron from '@mui/icons-material/ChevronRight'
+import { Listing, ListingStatus } from 'onecore-types'
+import { Link } from 'react-router-dom'
+
+import { DataGridTable } from '../../components'
+import { ListingWithOffer } from '../ParkingSpaces/hooks/useParkingSpaceListings'
+import { DeleteListing } from '../ParkingSpaces/components/DeleteListing'
+import { CreateApplicantForListing } from '../ParkingSpaces/components/create-applicant-for-listing/CreateApplicantForListing'
+
+const dateFormatter = new Intl.DateTimeFormat('sv-SE', {
+  timeZone: 'UTC',
+})
+
+const numberFormatter = new Intl.NumberFormat('sv-SE', {
+  style: 'currency',
+  currency: 'SEK',
+})
 
 const PublishParkingSpacesPage: React.FC = () => {
-  return <div>Publish Parking Spaces</div>
+  return (
+    <Listings
+      columns={getColumns(dateFormatter, numberFormatter).concat(
+        getActionColumns()
+      )}
+      rows={[]}
+      loading={false}
+      key="published"
+    />
+  )
+}
+
+const Listings = (props: {
+  columns: Array<GridColDef>
+  rows: Array<Listing>
+  loading: boolean
+}) => (
+  <DataGridTable
+    initialState={{
+      sorting: {
+        sortModel: [{ field: 'queuePoints', sort: 'desc' }],
+      },
+      pagination: { paginationModel: { pageSize: 30 } },
+    }}
+    pageSizeOptions={[10, 30, 60, 100]}
+    slots={{
+      noRowsOverlay: () => (
+        <Stack paddingTop="1rem" alignItems="center" justifyContent="center">
+          <Typography fontSize="14px">
+            Det finns inga annonser att visa.
+          </Typography>
+        </Stack>
+      ),
+    }}
+    columns={props.columns}
+    rows={props.rows}
+    getRowId={(row) => row.id}
+    loading={props.loading}
+    rowHeight={72}
+    disableRowSelectionOnClick
+    autoHeight
+  />
+)
+
+const getColumns = (
+  dateFormatter: Intl.DateTimeFormat,
+  numberFormatter: Intl.NumberFormat
+): Array<GridColDef<ListingWithOffer>> => {
+  const sharedColumnProps = {
+    editable: false,
+    flex: 1,
+  }
+
+  return [
+    {
+      field: 'address',
+      headerName: 'Bilplats',
+      ...sharedColumnProps,
+      flex: 1.25,
+      renderCell: ({ row }) => (
+        <span>
+          <span style={{ display: 'block' }}>{row.address}</span>
+          {row.rentalObjectCode}
+        </span>
+      ),
+    },
+    {
+      field: 'districtCaption',
+      headerName: 'Område',
+      ...sharedColumnProps,
+    },
+    {
+      field: 'objectTypeCaption',
+      headerName: 'Bilplatstyp',
+      ...sharedColumnProps,
+    },
+    {
+      field: 'monthlyRent',
+      headerName: 'Hyra',
+      ...sharedColumnProps,
+      valueFormatter: ({ value }) => `${numberFormatter.format(value)}/mån`,
+    },
+
+    // TODO: Antal publicerade intern kö
+  ]
+}
+
+const getActionColumns = (): Array<GridColDef<ListingWithOffer>> => {
+  return [
+    {
+      field: 'actions',
+      type: 'actions',
+      flex: 1,
+      minWidth: 250,
+      cellClassName: 'actions',
+      getActions: ({ row }) => [
+        <DeleteListing
+          key={0}
+          address={row.address}
+          rentalObjectCode={row.rentalObjectCode}
+          disabled={row.status !== ListingStatus.Active}
+          id={row.id}
+        />,
+        <CreateApplicantForListing
+          key={1}
+          disabled={row.status !== ListingStatus.Active}
+          listing={row}
+        />,
+      ],
+    },
+    {
+      field: 'action-link',
+      headerName: '',
+      sortable: false,
+      filterable: false,
+      flex: 0.5,
+      disableColumnMenu: true,
+      renderCell: ({ id }) => (
+        <Link to={`/bilplatser/${id}`}>
+          <IconButton sx={{ color: 'black' }}>
+            <Chevron />
+          </IconButton>
+        </Link>
+      ),
+    },
+  ]
 }
 
 export default PublishParkingSpacesPage
