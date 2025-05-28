e5421d3bfcfdaf77f5b95815777b19daa60ab50a
Sven Johansson <sven.johansson@iteam.se>
2025-05-28 10:46:21 +0200

    uth-239: CommentSection UI + backend

--- packages/backend/src/api.ts
+++ packages/backend/src/api.ts
@@ -1,11 +1,13 @@
 import KoaRouter from '@koa/router'
 import { routes as propertyInfoRoutes } from './services/property-info-service'
+import { routes as commentsRoutes } from './services/leasing-service/comments'
 import { routes as contactsRoutes } from './services/leasing-service/contacts'
 import { routes as offersRoutes } from './services/leasing-service/offers'
 import { routes as listingsRoutes } from './services/leasing-service/listings'
 
 const router = new KoaRouter()
 
+commentsRoutes(router)
 contactsRoutes(router)
 offersRoutes(router)
 listingsRoutes(router)
