b0646e2322733e815655bbc6a027172a05bda8d6
0x4d <0x4d@undefined>
2025-06-24 14:17:01 +0200

    Fix typo in theme styles: update 'backgrund' to 'backgroundColor' and adjust color values

--- packages/frontend/src/theme.ts
+++ packages/frontend/src/theme.ts
@@ -232,8 +232,8 @@ export const mdTheme = createTheme(
             style: {
               textTransform: 'none',
               fontWeight: 700,
-              backgrund: 'rgba(0, 0, 0, 1)',
-              color: 'rgba(255, 255, 255, 1)',
+              backgroundColor: 'black',
+              color: 'white',
               transition: 'none',
               ':hover': {
                 background: 'black',
