873288655ca2422c961f937869bba5b838b01b30
0x4d <0x4d@undefined>
2025-06-24 14:22:15 +0200

    Update button text in DeleteListing component from 'Ta bort annons' to 'Radera'

--- packages/frontend/src/pages/ParkingSpaces/components/DeleteListing.tsx
+++ packages/frontend/src/pages/ParkingSpaces/components/DeleteListing.tsx
@@ -48,7 +48,7 @@ export const DeleteListing = (
         variant="dark"
         onClick={() => setOpen(true)}
       >
-        Ta bort annons
+        Radera
       </Button>
       <Dialog
         onClose={onClose}
